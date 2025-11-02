0eab23833ea2f92618082d72af51c8358c0bef92
Pranish Bista <pranish@sysqube.com.np>
2025-11-02 18:01:47 +0545

    Auth added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import { StackProvider, StackTheme } from "@stackframe/stack";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { stackClientApp } from "../stack/client";
 import "./globals.css";
-import { NavBar } from "@/components/nav/nav-bar";
+import NavBar from "@/components/nav/nav-bar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        {children}
+        <StackProvider app={stackClientApp}>
+          <StackTheme>
+            <NavBar />
+            {children}
+          </StackTheme>
+        </StackProvider>
       </body>
     </html>
   );
